3b95b005d29b7de4146f8b751c195232bd957546
eryalito <eryalito@gmail.com>
2026-02-28 01:19:13 +0100

    allow categories with duplicated names
    Signed-off-by: eryalito <eryalito@gmail.com>

--- internal/models/category.go
+++ internal/models/category.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Name  string `gorm:"column:name;size:100;not null;uniqueIndex" json:"name"`
+	Name  string `gorm:"column:name;size:100;not null" json:"name"`
 	Color string `gorm:"column:color;size:20;not null" json:"color"`
 }
